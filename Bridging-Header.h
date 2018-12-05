bd0180eaa63cc61fc94a9c3e6fe80af58706b884
fatDoge <shenjulian777@gmail.com>
2018-12-05 22:57:21 +0800

    few hours of work

---
+++ Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  chitchat
+//
+//  Created by Shen Dennis on 5/12/2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+#import <JSQMessagesViewController/JSQMessages.h>
+
+#endif /* Bridging_Header_h */
