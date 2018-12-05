f0d63f28fc676fa9317d11cdd2206e64786bd0bb
student <student@cpe-172-100-19-23.twcny.res.rr.com>
2018-12-05 16:20:09 +0800

    wow such file many mass

---
+++ chitchat/ChatViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chitchat
+//
+//  Created by student on 5/12/2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
